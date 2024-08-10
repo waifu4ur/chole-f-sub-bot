bfa7c14ccd6c2ecb8df86f902a759ee9012d34ce
waifu4ur <153315802+waifu4ur@users.noreply.github.com>
2024-08-10 21:26:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,7 +12,7 @@ class Config(object):
 	SHORTLINK_API = os.environ.get('SHORTLINK_API')
 	BOT_OWNER = int(os.environ.get("BOT_OWNER", "7102263732"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001941319109")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002026961037")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
